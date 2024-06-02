a206315c52d02cc87375468b5df428d84de7de1e
ohjaoh <ohja1004@naver.com>
2024-06-03 00:16:59 +0900

    첫화면에 디자인추가 + index.html은 캐시를 사용할 수 있게 예외처리함

--- test/src/main/java/com/test/controller/CacheControlFilter.java
+++ test/src/main/java/com/test/controller/CacheControlFilter.java
@@ -1,9 +1,9 @@
 package com.test.controller;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,8 +11,15 @@ public class CacheControlFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
-        expireCache(httpResponse);
+
+        // 특정 URL 패턴에 대해 캐시 예외 처리
+        String requestURI = httpRequest.getRequestURI();
+        if (!isCacheExempt(requestURI)) {
+            expireCache(httpResponse);
+        }
+
         chain.doFilter(request, response);
     }
 
@@ -20,5 +27,10 @@ public class CacheControlFilter implements Filter {
         response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
         response.setHeader("Pragma", "no-cache");
         response.setDateHeader("Expires", 0);
+    }
+
+    private boolean isCacheExempt(String requestURI) {
+        // 캐시 예외 처리를 할 URL 패턴 정의
+        return requestURI.startsWith("/index") ;
     }
 }
