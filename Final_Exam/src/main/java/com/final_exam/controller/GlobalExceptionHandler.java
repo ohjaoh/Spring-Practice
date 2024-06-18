d702e158ed0f2da0c686d93f0209aff1ab786269
ohjaoh <ohja1004@naver.com>
2024-06-18 20:08:06 +0900

    [오자현] get으로 잘못된 요청을 하면 404페이지로 연결

---
+++ Final_Exam/src/main/java/com/final_exam/controller/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.final_exam.controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFound() {
+        return "404";
+    }
+}
